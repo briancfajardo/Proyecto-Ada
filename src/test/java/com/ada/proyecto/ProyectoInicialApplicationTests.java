20ae60904a620144126196c8793c8840d2c39c9d
Brian Camilo Fajardo Sánchez <briancfajardo@gmail.com>
2024-02-03 16:00:46 -0500

    Initial Rest controller

---
+++ src/test/java/com/ada/proyecto/ProyectoInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInicialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
